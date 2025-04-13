577385504ddb3d042d5d37b06e4556a4d6377255
Agusgod <91232860+AgusGodd@users.noreply.github.com>
2025-04-12 21:42:23 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import PlayerSetup from "../components/PlayerSetup";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-200 to-orange-100 text-black p-4">
+      <PlayerSetup />
+    </div>
+  );
+}
