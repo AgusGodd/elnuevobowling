9dabc9f69432c7a492266abc25faa358aacbbb44
Agusgod <91232860+AgusGodd@users.noreply.github.com>
2025-04-12 22:25:10 -0300

    Create gamescreen.js

---
+++ pages/gamescreen.js
@@ -0,0 +1,6 @@
+// pages/gamescreen.js
+import GameScreen from '../components/GameScreen';  // Importas el componente de GameScreen
+
+export default function GameScreenPage() {
+  return <GameScreen />;  // Usas el componente dentro de la página
+}
