098cb27beef7f3dd76088d4f329dcb642e405f60
Agusgod <91232860+AgusGodd@users.noreply.github.com>
2025-04-12 22:25:27 -0300

    Create finalscreen.js

---
+++ pages/finalscreen.js
@@ -0,0 +1,6 @@
+// pages/finalscreen.js
+import FinalScreen from '../components/FinalScreen';  // Importas el componente de FinalScreen
+
+export default function FinalScreenPage() {
+  return <FinalScreen />;  // Usas el componente dentro de la página
+}
