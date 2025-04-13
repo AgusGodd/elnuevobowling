e782556ed714d28a214409d71c943bd99e935144
Agusgod <91232860+AgusGodd@users.noreply.github.com>
2025-04-12 21:41:52 -0300

    Delete ElNuevoBowling (1) directory

--- ElNuevoBowling (1)/pages/index.js
+++
@@ -1,9 +0,0 @@
-import PlayerSetup from "../components/PlayerSetup";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-200 to-orange-100 text-black p-4">
-      <PlayerSetup />
-    </div>
-  );
-}
